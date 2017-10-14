b0460d847ce5d9d0a542d4266899d96e34ea56b9
wpeery <wpeery@users.noreply.github.com>
2017-10-13 21:37:57 -0700

    Update Searcher.py

--- Searcher.py
+++ Searcher.py
@@ -5,7 +5,7 @@ class Searcher:
     """Searches for a food using the gov api key"""
 
     def __init__(self):                            ##TODO PASS API KEY THROUGH CONSTRUCTOR
-        self.gov_api_key = "9ZH2gqcl0QxJfd7aqlhaeIdckvl0ha3qFWnHtVUH"  # api key goes here
+        self.gov_api_key = "API KEY GOES HERE"  # api key goes here
 
     def requestByName(self, foodName):
         url = "http://api.nal.usda.gov/ndb/search/?format=json&q=" + foodName + "&sort=r&max=25&offset=0&api_key=" + self.gov_api_key
